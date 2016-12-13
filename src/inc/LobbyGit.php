5ae3e8cbb3b44399a50955de6cb17b510a44a7e0
Subin Siby <subins2000@gmail.com>
2016-12-13 08:11:54 +0530

    Prefer PNG logos

--- src/inc/LobbyGit.php
+++ src/inc/LobbyGit.php
@@ -17,25 +17,25 @@ class LobbyGit {
             "last_commit" => null,
             "updated" => 0
           );
-  
+
   public function __construct($id, $git_url, $cloud_id = null){
     $this->id = $id;
     $this->git_url = $git_url;
     $this->git_dir = \Lobby\FS::loc("/contents/apps/lobby-server/src/data/git-cache/$id");
     $this->cloud_id = $cloud_id;
   }
-  
+
   public function register(){
     $sql = \Lobby\DB::getDBH()->prepare("SELECT * FROM `git_cache` WHERE `git_url` = ?");
     $sql->execute(array($this->git_url));
-    
+
     if($sql->rowCount() === 0){
       $sql = \Lobby\DB::getDBH()->prepare("INSERT INTO `git_cache` (`git_url`, `last_commit`, `updated`) VALUES(?, '', '0')");
       $sql->execute(array($this->git_url));
     }else{
       $this->info = $sql->fetch(\PDO::FETCH_ASSOC);
     }
-    
+
     /**
      * Only update if last update was 1 minute ago
      */
@@ -43,7 +43,7 @@ class LobbyGit {
       if($this->getRepo()){
         $sql = \Lobby\DB::getDBH()->prepare("UPDATE `git_cache` SET `updated` = UNIX_TIMESTAMP() WHERE `git_url` = ?");
         $sql->execute(array($this->git_url));
-        
+
         $this->info["updated"] = time();
         return true;
       }else{
@@ -53,21 +53,21 @@ class LobbyGit {
       return false;
     }
   }
-  
+
   public function update(){
     $sql = \Lobby\DB::getDBH()->prepare("UPDATE `git_cache` SET `updated` = '0', `last_commit` = '' WHERE `git_url` = ?");
     $sql->execute(array($this->git_url));
-    
+
     return $this->register();
   }
-  
+
   public function getRepo(){
     if(file_exists($this->git_dir))
       $this->recursiveRemoveDirectory($this->git_dir);
-    
+
     $repo = Gitonomy\Git\Admin::cloneTo($this->git_dir, $this->git_url, false);
     $tags = $repo->getReferences()->getTags();
-    
+
     /**
      * If no tags, use master
      */
@@ -77,61 +77,64 @@ class LobbyGit {
       rsort($tags);
       $commitHash = $tags[0]->getCommitHash();
     }
-    
+
     if($commitHash === $this->info["last_commit"]){
       // No need of update
       return true;
     }
-    
+
     $sql = \Lobby\DB::getDBH()->prepare("UPDATE `git_cache` SET `last_commit` = ? WHERE `git_url` = ?");
     $sql->execute(array($commitHash, $this->git_url));
-    
+
     $this->recursiveRemoveDirectory($this->git_dir . "/.git");
-    
+
     /**
      * Get screenshots
      */
     $manifest = json_decode(file_get_contents($this->git_dir . "/manifest.json"), true);
-    
+
     // Manifest file is invalid
     if(!is_array($manifest))
       return false;
-    
+
     if(isset($manifest["version"]))
       $latestVersion = $manifest["version"];
     else
       return false;
-    
+
     if(isset($manifest->screenshots)){
       $screenshots = array();
       foreach($manifest["screenshots"] as $img){
         $url = parse_url($img);
         if($url["host"] === "i.imgur.com"){
           $path = $url["path"];
-          
+
           // $path has slash at beginning
           $headers = @get_headers("http://i.imgur.com$path", 1);
-          
+
           if($headers[0] === "HTTP/1.1 200 OK"){
             $screenshots[] = "//i.imgur.com$path";
           }
         }
       }
-      
+
       if(empty($screenshots))
         unset($screenshots);
     }
-    
+
     if(exec("cd {$this->git_dir};zip -r '{$this->git_dir}/app.zip' ./ -1 -q;") !== false){
       $logo = true;
-      
-      if(file_exists($this->git_dir . "/src/image/logo.svg"))
-        $this->convertLogoToPNG();
-      else if(file_exists($this->git_dir . "/src/image/logo.png"))
+
+      /**
+       * use PNG image if available
+       */
+      if(file_exists($this->git_dir . "/src/image/logo.png"))
         copy($this->git_dir . "/src/image/logo.png", $this->git_dir . "/logo.png");
+      else if(file_exists($this->git_dir . "/src/image/logo.svg"))
+        $this->convertLogoToPNG();
       else
         $logo = false;
-     
+
       $webdavPass = getenv("SKY_WEBDAV_PASS");
       $settings = array(
         'baseUri' => "https://sky-phpgeek.rhcloud.com/remote.php/webdav/Apps/{$this->id}/",
@@ -139,19 +142,19 @@ class LobbyGit {
         'password' => $webdavPass
       );
       $client = new Client($settings);
-      
+
       /**
        * Create folder
        */
       $client->request('MKCOL');
-      
+
       /**
        * Upload files
        */
       $client->request('PUT', "{$this->id}.zip", file_get_contents($this->git_dir . "/app.zip"));
       if($logo)
         $client->request('PUT', "logo.png", file_get_contents($this->git_dir . "/logo.png"));
-      
+
       $request = \Requests::post("https://sky-phpgeek.rhcloud.com/ocs/v1.php/apps/files_sharing/api/v1/shares?format=json", array(
         "Content-Type" => "application/x-www-form-urlencoded"
       ), array(
@@ -162,20 +165,20 @@ class LobbyGit {
       ));
       $response = json_decode($request->body);
       $this->cloud_id = $response->ocs->data->token;
-      
+
       $extraColumnData = array(
         "short_description" => $manifest["short_description"]
       );
-      
+
       if(isset($latestVersion))
         $extraColumnData["version"] = $latestVersion;
-      
+
       if(isset($screenshots))
         $extraColumnData["screenshots"] = implode("\n", $screenshots);
-        
+
       if(isset($manifest["require"]) && is_array($manifest["require"]))
         $extraColumnData["requires"] = json_encode($manifest["require"]);
-      
+
       /**
        * Update Cloud ID and download file size
        */
@@ -184,7 +187,7 @@ class LobbyGit {
         ":downloadSize" => filesize($this->git_dir . "/app.zip"),
         ":appID" => $this->id
       );
-      
+
       $extraColumns = "";
       if(!empty($extraColumnData)){
         foreach($extraColumnData as $k => $v){
@@ -192,20 +195,20 @@ class LobbyGit {
           $appInfoUpdate[":$k"] = $v;
         }
       }
-      
+
       $sql = \Lobby\DB::getDBH()->prepare("UPDATE `apps` SET `cloud_id` = :cloudID, `download_size` = :downloadSize, `updated` = NOW() $extraColumns WHERE `id` = :appID");
       $sql->execute($appInfoUpdate);
-      
+
       $this->recursiveRemoveDirectory($this->git_dir);
       return true;
     }
     return false;
   }
-  
+
   public function download(){
     header("Location: {$this->cloud_url}/s/{$this->cloud_id}/download?path=%2F&files={$this->id}.zip");
   }
-  
+
   public function logo($logo = 1){
     if($this->cloud_id == null || $logo == 0){
       header("Location: ". L_URL ."/contents/apps/lobby-server/src/image/blank.png");
@@ -213,7 +216,7 @@ class LobbyGit {
       header("Location: {$this->cloud_url}/s/{$this->cloud_id}/download?path=%2F&files=logo.png");
     }
   }
-  
+
   /**
    * Convert SVG to PNG
    */
@@ -222,7 +225,7 @@ class LobbyGit {
     $output = $this->git_dir . "/logo.png";
     exec("java -Djava.security.policy=='". __DIR__ ."/svg-converter/java.policy' -jar '" . __DIR__ . "/svg-converter/batik-rasterizer.jar' -d '$output' -m image/png '$svg'");
   }
-  
+
   /**
    * Recursive Directory Remover
    */
@@ -245,5 +248,5 @@ class LobbyGit {
       rmdir($dir);
     }
   }
-  
+
 }
